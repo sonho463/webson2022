f1de969ad999f18112eea14464b08215ec318805
Son Hong Su <son.ho463@gmail.com>
2023-01-28 11:32:50 +0900

    実績部分mdで

---
+++ theme/src/config/work.js
@@ -0,0 +1,23 @@
+export const work = [
+	{
+		title: '建設会社のホームページ',
+		desc: 'Photohopデザインカンプからコーディング。トップページではスライダー＋アニメーションを実装しました。 ',
+		imageUrl: 'const-company_mockuper',
+	},
+	{
+		title: 'ブルーリーフパートナーズ様LP',
+		desc: 'Photohopデザインカンプからコーディング。トップページではスライダー＋アニメーションを実装しました。 ',
+		imageUrl: 'blue-leaf_mockup',
+	},
+	{
+		title: '英会話＋フィットネス',
+		desc: 'Photohopデザインカンプからコーディング。トップページではスライダー＋アニメーションを実装しました。 ',
+		imageUrl: 'eng_mockup',
+	},
+	{
+		title: '英会話スクール（WordPress）',
+		desc: 'Photohopデザインカンプからコーディング。トップページではスライダー＋アニメーションを実装しました。 ',
+		imageUrl: 'eng-sch_mockup',
+	},
+
+];
